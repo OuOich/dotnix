54480a32bc3654ce77599b6ae9aa62312a6b7314
Cheng <chengcheng@miao.ms>
2026-01-16 22:43:55 +0800

    feat(@mochi): remove the `cheng@taco` ssh key access from root user

--- nixos/hosts/mochi/users.nix
+++ nixos/hosts/mochi/users.nix
@@ -5,7 +5,6 @@
     root = {
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBCfZ2IPPTxJz+hBod0mwsLfIBlBgeam87+LPQqN/DfD cheng@mochi"
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDK2uKnIK1KU3FSnHKplbTxxxqOOGdJg3/pqGow1CUUO cheng@taco"
       ];
     };
 
