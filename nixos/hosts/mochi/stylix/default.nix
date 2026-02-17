1b27eef4ad634e387ad13520405b158ade6c0581
Cheng <chengcheng@miao.ms>
2026-02-17 11:20:40 +0800

    feat(@mochi): disable stylix by default

--- nixos/hosts/mochi/stylix/default.nix
+++ nixos/hosts/mochi/stylix/default.nix
@@ -4,5 +4,11 @@
   stylix = {
     enable = true;
     base16Scheme = "${self}/assets/base16-schemes/carbonfox.yaml";
+
+    autoEnable = false;
+    targets = {
+      console.enable = true;
+      grub.enable = true;
+    };
   };
 }
