3dd069b8fe84b98cc43c7fbaf359704a7700ed3f
Cheng <chengcheng@miao.ms>
2026-02-25 14:17:20 +0800

    feat(cheng@_): configure catppuccin for lazygit

--- nixos/home/cheng/programs/lazygit/default.nix
+++ nixos/home/cheng/programs/lazygit/default.nix
@@ -1,18 +1,35 @@
 {
-  programs.lazygit = {
-    enable = true;
+  config,
+  lib,
+  ...
+}:
 
-    settings = {
-      git = {
-        overrideGpg = true;
-      };
+lib.mkMerge [
+  {
+    programs.lazygit = {
+      enable = true;
 
-      gui = {
-        authorColors = {
-          "Cheng" = "magenta";
-          "成成0v0" = "magenta";
+      settings = {
+        git = {
+          overrideGpg = true;
+        };
+
+        gui = {
+          authorColors = {
+            "Cheng" = "magenta";
+            "Cheng :3" = "magenta";
+            "成成0v0" = "magenta";
+          };
         };
       };
     };
-  };
-}
+  }
+
+  (lib.mkIf (lib.strings.hasPrefix "catppuccin-" config.settings.theme.colorscheme) {
+    stylix.targets.lazygit.enable = false;
+    catppuccin.lazygit = {
+      enable = true;
+      accent = "blue";
+    };
+  })
+]
