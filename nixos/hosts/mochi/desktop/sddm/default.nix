aa485b337514fa7438a8825ff0fcefc42095b22f
Cheng <chengcheng@miao.ms>
2026-03-01 20:31:44 +0800

    feat(@mochi): persist sddm state (`/var/lib/sddm/state.conf`)

--- nixos/hosts/mochi/desktop/sddm/default.nix
+++ nixos/hosts/mochi/desktop/sddm/default.nix
@@ -1,27 +1,45 @@
-{ pkgs, ... }:
-
-let
-  sddm-astronaut = pkgs.sddm-astronaut.override {
-    embeddedTheme = "hyprland_kath";
-    themeConfig = {
-      AllowUppercaseLettersInUsernames = "true";
-    };
-  };
-in
 {
-  services.displayManager.sddm = {
-    enable = true;
-    package = pkgs.kdePackages.sddm;
-    extraPackages = [ sddm-astronaut ];
+  config,
+  options,
+  pkgs,
+  lib,
+  ...
+}:
 
-    wayland.enable = true;
+lib.mkMerge [
+  (
+    let
+      sddm-astronaut = pkgs.sddm-astronaut.override {
+        embeddedTheme = "hyprland_kath";
+        themeConfig = {
+          AllowUppercaseLettersInUsernames = "true";
+        };
+      };
+    in
+    {
+      services.displayManager.sddm = {
+        enable = true;
+        package = pkgs.kdePackages.sddm;
+        extraPackages = [ sddm-astronaut ];
 
-    enableHidpi = true;
-    theme = "sddm-astronaut-theme";
-  };
+        wayland.enable = true;
 
-  environment.systemPackages = with pkgs; [
-    sddm-astronaut
-    kdePackages.qtmultimedia
-  ];
-}
+        enableHidpi = true;
+        theme = "sddm-astronaut-theme";
+      };
+
+      environment.systemPackages = with pkgs; [
+        sddm-astronaut
+        kdePackages.qtmultimedia
+      ];
+    }
+  )
+
+  (lib.mkIf (options.environment ? persistence) {
+    environment.persistence.${config.fileSystems."/persist".mountPoint} = {
+      files = [
+        "/var/lib/sddm/state.conf"
+      ];
+    };
+  })
+]
