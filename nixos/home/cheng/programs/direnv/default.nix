8c479b66c35d02823cb6a7332d8542bf05549a0b
Cheng <chengcheng@miao.ms>
2026-05-10 15:46:00 +0800

    fix(cheng@_): persist direnv memories

--- nixos/home/cheng/programs/direnv/default.nix
+++ nixos/home/cheng/programs/direnv/default.nix
@@ -1,10 +1,27 @@
 {
-  programs.direnv = {
-    enable = true;
-    nix-direnv.enable = true;
+  options,
+  osConfig,
+  lib,
+  ...
+}:
 
-    config = {
-      hide_env_diff = true;
+lib.mkMerge [
+  {
+    programs.direnv = {
+      enable = true;
+      nix-direnv.enable = true;
+
+      config = {
+        hide_env_diff = true;
+      };
     };
-  };
-}
+  }
+
+  (lib.optionalAttrs (options.home ? persistence) {
+    home.persistence.${osConfig.fileSystems."/persist".mountPoint} = {
+      directories = [
+        ".local/share/direnv"
+      ];
+    };
+  })
+]
