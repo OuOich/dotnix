e9b11b5187e334faeaeed756e2993698940f60eb
Cheng <chengcheng@miao.ms>
2026-05-12 05:16:29 +0800

    fix(cheng@_): persist zoxide jump history

--- nixos/home/cheng/programs/zoxide/default.nix
+++ nixos/home/cheng/programs/zoxide/default.nix
@@ -1,5 +1,21 @@
 {
-  programs.zoxide = {
-    enable = true;
-  };
-}
+  options,
+  lib,
+  ...
+}:
+
+lib.mkMerge [
+  {
+    programs.zoxide = {
+      enable = true;
+    };
+  }
+
+  (lib.optionalAttrs (options.home ? persistence) {
+    home.persistence."/persist" = {
+      directories = [
+        ".local/share/zoxide"
+      ];
+    };
+  })
+]
